3a8d817881e9851e980b4a775dee20e72e2b833a
Sumit Jadhav <summit.jadhav@gmail.com>
2023-12-04 17:16:17 +0530

    Initial commit - Setup react application

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// STYLE
+import style from './HomePage.module.scss'
+
+const HomePage: React.FC = () => {
+    return (
+        <div className={style.component}>
+            <h1>This is Home Page</h1>
+        </div>
+    )
+}
+
+export default HomePage
