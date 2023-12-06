81a143248eb0fca1b4562bc85c746b435f781415
Sumit Jadhav <summit.jadhav@gmail.com>
2023-12-06 11:57:13 +0530

    Test hash router

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,23 @@ import React from 'react'
 
 // STYLE
 import style from './HomePage.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage: React.FC = () => {
+    const navigate = useNavigate()
+
+    const event = {
+        redirectToHomePage: () => {
+            navigate("/dsds")
+        }
+    }
+
     return (
         <div className={style.component}>
             <h1>This is Sumit&apos;s Portfolio</h1>
+            <button onClick={() => event.redirectToHomePage()}>
+                Go Away
+            </button>
         </div>
     )
 }
