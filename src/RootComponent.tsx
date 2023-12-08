a236b19c0dbcc39189843b5d6073beb93f77270c
Sumit Jadhav <summit.jadhav@gmail.com>
2023-12-08 15:41:04 +0530

    Add fancy cursor

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { MemoryRouter as Router, Route, Routes, Outlet } from 'react-router-dom'
 import { BubblyContainer } from 'react-bubbly-transitions'
+import AnimatedCursor from 'react-animated-cursor'
 import { ROUTES } from './resources/routes-constants'
 import { About, Contact, Home, Nav } from './components'
 import './styles/main.scss'
 
 const RootComponent: React.FC = () => {
     return (
-        <Router>
+        <Router basename="/">
             <BubblyContainer />
             <Routes>
                 <Route
                     path="/"
                     element={
                         <>
+                            <AnimatedCursor />
                             <Nav />
                             <Outlet />
                         </>
