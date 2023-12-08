2d1fcea78c6b73b2d632e140a6d22744828b2ebc
Sumit Jadhav <summit.jadhav@gmail.com>
2023-12-08 17:42:27 +0530

    Add photo

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import image from '../../images/Sumit.jpg'
 
 export const Home: React.FC = () => {
     return (
         <div className='page'>
+            <img className='tile' src={image} width={500}></img>
             <div className='hero-section'>
                 <h2>I am <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/sumit8855/' className='highlight'>Sumit Jadhav</a>, Software Consultant based in Pune, having expertise in front-end and backend development.</h2>
             </div>
