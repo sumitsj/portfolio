eb1aef898378612145603abccda7a1f22c42e377
Sumit Jadhav <summit.jadhav@gmail.com>
2023-12-11 00:04:30 +0530

    Cosmetic changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,10 +3,11 @@ import image from '../../images/Sumit.jpg'
 
 export const Home: React.FC = () => {
     return (
-        <div className='page'>
-            <img className='tile' src={image} width={500}></img>
-            <div className='hero-section'>
-                <h2>I am <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/sumit8855/' className='highlight'>Sumit Jadhav</a>, Software Consultant based in Pune, having expertise in front-end and backend development.</h2>
+        <div className='page hero-section-image'>
+            {/* <img className='tile' src={image} width={500}></img> */}
+            <div className='hero-section text-shadow'>
+                <h1><a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/sumit8855/' className='highlight'>Sumit Jadhav</a></h1>
+                <h2>Software Consultant, Pune (India)</h2>
             </div>
         </div>
     )
